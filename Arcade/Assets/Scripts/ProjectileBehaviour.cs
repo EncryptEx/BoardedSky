7c1ddaf68d875bf743486a1aa5a31d996cda215c
EncryptEx <limusina10@gmail.com>
2021-07-01 15:53:40 +0200

    Basic gameplay.
    But ball can bug bouncing forever.

--- Arcade/Assets/Scripts/ProjectileBehaviour.cs
+++ Arcade/Assets/Scripts/ProjectileBehaviour.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileBehaviour : MonoBehaviour
-{
+{   public float initialForce = -200f;
+    public float random = 50f;
+    public Vector3 startPos;
+    private Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
+        //set start pos
+        transform.position = startPos;
+        //rotate and move
+        rb.AddForce(new Vector3(random,0f,initialForce));
     }
 
     // Update is called once per frame
