f351adb40b37af01ee0171c497f6da05ba8c396c
EncryptEx <limusina10@gmail.com>
2021-07-16 19:47:27 +0200

    lives system display working

--- Arcade/Assets/Scripts-GAME/LivesManager.cs
+++ Arcade/Assets/Scripts-GAME/LivesManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +14,74 @@ public class LivesManager : MonoBehaviour
     
     [Tooltip("GameOver Script")]
     public GameOverScript go;
-    
+
+    //hearts link.
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+    public GameObject Gheart1;
+    public GameObject Gheart2;
+    public GameObject Gheart3;
+
+
+
+    private void Update()
+    {
+        //display hearts in function of lives var.
+
+        switch (lives)
+        {
+            case 5: //6 lives, max. 
+                heart1.SetActive(false);
+                heart2.SetActive(false);
+                heart3.SetActive(false);
+                Gheart1.SetActive(true);
+                Gheart2.SetActive(true);
+                Gheart3.SetActive(true);
+                break;
+            case 4: //5 lives
+                heart1.SetActive(false);
+                heart2.SetActive(false);
+                heart3.SetActive(true);
+                Gheart1.SetActive(true);
+                Gheart2.SetActive(true);
+                Gheart3.SetActive(false);
+                break;
+            case 3: //4 lives
+                heart1.SetActive(false);
+                heart2.SetActive(true);
+                heart3.SetActive(true);
+                Gheart1.SetActive(true);
+                Gheart2.SetActive(false);
+                Gheart3.SetActive(false);
+                break;
+            case 2: //3 lives.
+                heart1.SetActive(true);
+                heart2.SetActive(true);
+                heart3.SetActive(true);
+                Gheart1.SetActive(false);
+                Gheart2.SetActive(false);
+                Gheart3.SetActive(false);
+                break;
+            case 1: // 2 lives
+                heart1.SetActive(true);
+                heart2.SetActive(true);
+                heart3.SetActive(false);
+                Gheart1.SetActive(false);
+                Gheart2.SetActive(false);
+                Gheart3.SetActive(false);
+                break;
+            case 0: //1 live
+                heart1.SetActive(true);
+                heart2.SetActive(false);
+                heart3.SetActive(false);
+                Gheart1.SetActive(false);
+                Gheart2.SetActive(false);
+                Gheart3.SetActive(false);
+                break;
+        }
+    }
+
     public void UserHasLostABall()
     {
         Debug.Log("Action Recieved.");
