fd006147fb4cea13c0bca32ccb76fb9b1e187616
EncryptEx <limusina10@gmail.com>
2021-07-16 13:13:02 +0200

    Audio finally working

--- Arcade/Assets/Scripts-MENU/MainMenu.cs
+++ Arcade/Assets/Scripts-MENU/MainMenu.cs
@@ -5,14 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private AudioManager am;
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        StartCoroutine(AudioManager.Instance.FadeOut());
+        am = GameObject.Find("SoundSystem").GetComponent<AudioManager>();
+        StartCoroutine(FadeOut());
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+    
+    private IEnumerator FadeOut()
+    {
+        Debug.Log("STARTING FADEOUT");
+        while (am.audio.volume > 0f) //for future whould be user's predef. 
+        {
+            Debug.Log(am.audio.volume);
+            //Debug.Log("augmenting volume"+am.volume);
+            am.audio.volume -= 0.01f;
+            yield return null;
+        }
+
+        am.audio.volume = 0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
