4bd13d909429407cef17e91a804bf87ced30c6df
EncryptEx <limusina10@gmail.com>
2021-07-26 22:01:04 +0200

    Nitro UI added and working.

--- Arcade/Assets/Scripts-GAME/NitroManager.cs
+++ Arcade/Assets/Scripts-GAME/NitroManager.cs
@@ -7,14 +7,28 @@ public class NitroManager : MonoBehaviour
    
    public bool isNitroAvailable;
    private AudioManager am;
+
+   public GameObject nitroReady;
+   public GameObject nitroUsing;
+   public GameObject nitroEmpty;
+   
+   
    private void Start()
    {
       isNitroAvailable = true;
       am = AudioManager.Instance;
+      
+      // prepare ui 
+      nitroReady.SetActive(true);
+      nitroUsing.SetActive(false);
+      nitroEmpty.SetActive(false);
    }
 
    public void DisableNitro()
    {
+      nitroReady.SetActive(false);
+      nitroUsing.SetActive(true);
+      nitroEmpty.SetActive(false);
       isNitroAvailable = false;
       //do an animation
       //do sound
@@ -27,6 +41,14 @@ public class NitroManager : MonoBehaviour
    void DisableAnimation()
    {
       nitroAnimation.SetActive(false);
+      DisableUI();
+   }
+
+   void DisableUI()
+   {
+      nitroReady.SetActive(false);
+      nitroUsing.SetActive(false);
+      nitroEmpty.SetActive(true);
    }
    
 }
